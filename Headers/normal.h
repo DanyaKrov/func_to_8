64924a921aafbffeec8000653c14c61d089a5dbe
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-04 00:34:42 +0300

    headers

---
+++ Headers/normal.h
@@ -0,0 +1,15 @@
+#ifndef NORMAL_H_INCLUDED
+#define NORMAL_H_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+int main();
+string if_pos(int num, int type);
+string if_neg(int num, int type);
+string func_to8_Str(int num);
+string func_to8_Rev(int num);
+string func_to8_Add(int num);
+int pow_but_cooler(int a, int deg);
+
+#endif // NORMAL_H_INCLUDED
